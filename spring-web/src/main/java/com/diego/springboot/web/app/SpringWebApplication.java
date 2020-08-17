ca965f98c079f875e2f2ba81511044f0d9e3dfb7
oiego <diegomenesest@gmail.com>
2020-08-17 03:31:34 -0500

    Spring files

--- spring-web/src/main/java/com/diego/springboot/web/app/SpringWebApplication.java
+++ spring-web/src/main/java/com/diego/springboot/web/app/SpringWebApplication.java
@@ -3,6 +3,7 @@ package com.diego.springboot.web.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringWebApplication {
 
--- spring-web/src/test/java/com/diego/springboot/web/app/SpringWebApplicationTests.java
+++ spring-web/src/test/java/com/diego/springboot/web/app/SpringWebApplicationTests.java
@@ -3,6 +3,7 @@ package com.diego.springboot.web.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SpringWebApplicationTests {
 
