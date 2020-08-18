5c77df33b8e888714dfd079cb508889ee89d4b91
oiego <diegomenesest@gmail.com>
2020-08-18 13:48:55 -0500

    index changes

--- spring-web/src/main/java/com/diego/springboot/web/app/controllers/IndexController.java
+++ spring-web/src/main/java/com/diego/springboot/web/app/controllers/IndexController.java
@@ -2,14 +2,18 @@ package com.diego.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
+@RequestMapping(value = "/app")
 public class IndexController {
 	
+	// Por defecto es GET
 //	@RequestMapping(value = "/", method = RequestMethod.GET) //Por defecto es GET. POST  pa formularios y PUT y DELETE para REST
-//	@PostMapping -> mismo q arriba  
-//	@DeleteMapping
-	@GetMapping(value = "/") // -> a shortcut for @RequestMapping(method = RequestMethod.GET)
+//	@PostMapping -> mismo q arriba pero método post   
+//	@DeleteMapping -> mismo pero para método delete
+	
+	@GetMapping(value = {"/index", "/", "/home"}) // -> a shortcut for @RequestMapping(method = RequestMethod.GET)
 	public String index() {
 		return "index";
 	}
